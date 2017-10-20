1a30f7c1c0eccaba468c9cb54d43ec77dc6bffa6
Linh Ng <mail2ngoclinh@gmail.com>
2017-10-20 16:11:32 +0700

    Initial Commit

---
+++ GPUImage_Playground_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPUImage_Playground_1
+//
+//  Created by Linh Ngoc on 10/20/17.
+//  Copyright © 2017 Linh Ngoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPUImage_Playground_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPUImage_Playground_1
+//
+//  Created by Linh Ngoc on 10/20/17.
+//  Copyright © 2017 Linh Ngoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
